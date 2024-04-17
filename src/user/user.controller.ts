4adc36425fc3fecb1d1de9b6ce141d7dec0b2d73
zenykhaidukk <zenukh2@gmail.com>
2024-04-17 17:04:01 +0300

    setup e2e and test db

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { JwtGuard } from '../auth/guard/jwt.guard';
-import { GetUser } from 'src/auth/decorator/get-user.decorator';
+import { GetUser } from '../auth/decorator/get-user.decorator';
 import { User } from '@prisma/client';
 
 @UseGuards(JwtGuard)
